8933f0597135e047a068d3e1a1bb3536dd68f393
uhqlA <alfie.alphonce@gmail.com>
2024-07-19 23:22:40 +0300

    working

--- djangoProject/api/models.py
+++ djangoProject/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    priority = models.IntegerField(default=0)
+    title = models.CharField(max_length=255, default="Untitled")
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
